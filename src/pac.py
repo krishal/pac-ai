c6043043d7c65a587fed54d6d822321842425c24
Keegan Fernandes <gungurlo@hotmail.com>
2016-03-24 00:09:57 -0400

    Fized Keyboard presses NEED TO CHANGE UP DOWN LEFT RIGHT INPUTS in EMULATOR TO 8 2 4 6 RESPECTIVELY (this is to mimic the up down left right arrow keys on the number pad)

--- src/pac.py
+++ src/pac.py
@@ -1,7 +1,65 @@
-rom PIL import ImageGrab
+from PIL import ImageGrab
 import win32gui
 import numpy as np
 import win32com.client as comclt
+import ctypes
+import time
+
+SendInput = ctypes.windll.user32.SendInput
+
+SC_UP = 0xC8
+SC_LEFT = 0x4B 
+SC_RIGHT = 0x4D
+SC_DOWN = 0x50
+
+# C struct redefinitions 
+PUL = ctypes.POINTER(ctypes.c_ulong)
+class KeyBdInput(ctypes.Structure):
+    _fields_ = [("wVk", ctypes.c_ushort),
+                ("wScan", ctypes.c_ushort),
+                ("dwFlags", ctypes.c_ulong),
+                ("time", ctypes.c_ulong),
+                ("dwExtraInfo", PUL)]
+
+class HardwareInput(ctypes.Structure):
+    _fields_ = [("uMsg", ctypes.c_ulong),
+                ("wParamL", ctypes.c_short),
+                ("wParamH", ctypes.c_ushort)]
+
+class MouseInput(ctypes.Structure):
+    _fields_ = [("dx", ctypes.c_long),
+                ("dy", ctypes.c_long),
+                ("mouseData", ctypes.c_ulong),
+                ("dwFlags", ctypes.c_ulong),
+                ("time",ctypes.c_ulong),
+                ("dwExtraInfo", PUL)]
+
+class Input_I(ctypes.Union):
+    _fields_ = [("ki", KeyBdInput),
+                 ("mi", MouseInput),
+                 ("hi", HardwareInput)]
+
+class Input(ctypes.Structure):
+    _fields_ = [("type", ctypes.c_ulong),
+                ("ii", Input_I)]
+
+# Actuals Functions
+
+def PressKey(hexKeyCode):
+    extra = ctypes.c_ulong(0)
+    ii_ = Input_I()
+    ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008, 0, ctypes.pointer(extra) )
+    x = Input( ctypes.c_ulong(1), ii_ )
+    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+def ReleaseKey(hexKeyCode):
+    extra = ctypes.c_ulong(0)
+    ii_ = Input_I()
+    ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008 | 0x0002, 0, ctypes.pointer(extra) )
+    x = Input( ctypes.c_ulong(1), ii_ )
+    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+
 
 def takeScreenshot():
 	toplist, winlist = [], []
@@ -33,10 +91,10 @@ def main():
 	
 	wsh= comclt.Dispatch("WScript.Shell")
 	wsh.AppActivate("jnes 1.1") # select another application
-	wsh.SendKeys("{UP}")
-	wsh.SendKeys("{DOWN}")
-	wsh.SendKeys("{LEFT}")
-	wsh.SendKeys("{RIGHT}") # send the keys you want
+	
+	PressKey(SC_UP)
+	time.sleep(0.2)
+	ReleaseKey(SC_UP)
 
 if __name__ == '__main__':
 	main()
